74df9345088e2f1aff67a5c724f56e7a8b32b85c
surya prakash Gadi <105710300+suryaprakash-Gadi@users.noreply.github.com>
2025-04-20 17:27:43 +0530

    Update cors.config.js

--- backend/config/cors.config.js
+++ backend/config/cors.config.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['https://minidigitaldiner.netlify.app', 'http://localhost:3000'], // ✅ Allow both dev and production
+  origin: ['your frontend_url', 'http://localhost:3000'], // ✅ Allow both dev and production
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
 };
